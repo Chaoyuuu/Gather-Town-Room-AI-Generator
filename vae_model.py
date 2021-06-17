69f2cdc545d7bb65ac5c29bceeafad36936f7fa6
Chaoyuuu <chaoyu2330@gmail.com>
2021-06-18 01:37:36 +0800

    add ignore chairs constrain

--- vae_model.py
+++ vae_model.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -58,11 +60,57 @@ def tensor_to_json(sample, threshold):
                 for row in range(10):
                     data = batch[i]
                     i += 1
-                    if data >= threshold:
+                    if depth == 15 or depth == 16:
+                        output_tensor[depth][col][row] = data
+                    elif data >= threshold:
                         output_tensor[depth][col][row] = data
 
     return encode_to_json(output_tensor, threshold)
 
+
+def decode_orientation(object_index, x, y):
+    down = {'x': 0, 'y': 1}
+    up = {'x': 0, 'y': -1}
+    left = {'x': -1, 'y': 0}
+    right = {'x': 1, 'y': 0}
+    orientation = [down, up, left, right]
+
+    two_direction_object = [2, 9]
+    one_direction_object = [0, 1, 3, 5, 13]
+    four_direction_object = [4, 6, 7, 8, 10, 11, 12, 14]
+
+    if object_index in one_direction_object:
+        return 0
+    elif object_index in two_direction_object:
+        up_length = abs(x - up.get('x')) ** 2 + abs(y - up.get('y')) ** 2
+        down_length = abs(x - down.get('x')) ** 2 + abs(y - down.get('y')) ** 2
+        if up_length < down_length:
+            return 2
+        return 0
+    elif object_index in four_direction_object:
+        min_length = 0
+        object_orientation = 0
+        for i in range(len(orientation)):
+            length = abs(x - orientation[i].get('x')) ** 2 + abs(y - orientation[i].get('y')) ** 2
+            if length < min_length:
+                min_length = length
+                object_orientation = i
+        return object_orientation
+    else:
+        print("error in orientation")
+        return 0
+
+
+def ignore_chair(index):
+    if index == 8 or index == 14:
+        return True
+    return True
+
+
+def shift_object(index, x):
+    if index == 9 or index == 2:
+        x -= 1
+    return x
 
 def encode_to_json(output_tensor, threshold):
     room_json = {
@@ -70,25 +118,22 @@ def encode_to_json(output_tensor, threshold):
         "room": []
     }
 
-    for col in range(13):
-        for row in range(10):
+    for row in range(13):
+        for col in range(10):
             max_value = 0
             max_index = -1
             for depth in range(15):
-                data = output_tensor[depth][col][row]
+                data = output_tensor[depth][row][col]
 
                 if data >= threshold and max_value < data:
                     max_index = depth
 
-            if max_index != -1:
+            if max_index != -1 and ignore_chair(max_index):
                 object_name = object_name_dict[max_index]
-                orientation = 0
+                orientation = decode_orientation(max_index, output_tensor[15][row][col], output_tensor[16][row][col])
                 object = copy.deepcopy(object_dict[object_name][orientation])
-                if max_index == 9 or max_index == 2:
-                    row -= 1
-                    col -= 1
-                object["x"] = row
-                object["y"] = col
+                object["x"] = shift_object(max_index, col)
+                object["y"] = row
                 room_json["room"].append(object)
 
     return room_json
