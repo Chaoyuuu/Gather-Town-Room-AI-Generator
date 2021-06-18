60d15555989e7c2440542131c1273314d6e23248
ningwang <nyngwang@gmail.com>
2021-06-18 21:50:36 +0800

    update 4.

--- GA/main_ga.py
+++ GA/main_ga.py
@@ -19,7 +19,7 @@ P = 0.0
 M_P1 = 0.5
 M_P2 = 0.2
 SURVIVE_PAIR = 1
-POP_SIZE = 30
+POP_SIZE = 10
 GEN_LIMIT = 30000
 WORK_SIZE = 30
 MIN_ITEMS = 10
@@ -28,7 +28,7 @@ MAX_ITEMS = 20
 WEIGHT_LIMIT = 20000
 # Constants (fitness2)
 SOME = 5
-GAN_BASELINE = 0.4
+GAN_BASELINE = 0.5
 DISS = Discriminator()
 
 # Mappings
@@ -283,7 +283,8 @@ def run_ga(
                 mutation_func2(child)
                 next_generation.append(child)
 
-        print(f"\tDISS={fitness_func(next_generation[0])}, room={pretty_print(next_generation[0])}")
+        print(f"\tDISS={fitness_func(next_generation[0])}")
+        pretty_print(next_generation[0])
         if some_pass_by_fitness(SOME, next_generation, fitness_func):
             print(f"Top {SOME} of population has passed GAN baseline={GAN_BASELINE}, finished!")
             break
